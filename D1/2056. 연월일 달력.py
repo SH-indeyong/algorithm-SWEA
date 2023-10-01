acefd65c23ac000c95b44e635a04ceb820c97000
SH-indeyong <ohsh1115@gmail.com>
2023-10-01 23:22:51 +0900

    pass

---
+++ D1/2056. 연월일 달력.py
@@ -0,0 +1,12 @@
+T = int(input())
+m_day = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+for test_case in range(1, T+1):
+    n = input()
+    y = n[:4]
+    m = n[4:6]
+    d = n[6:]
+    if (int(m) < 1 or int(m) > 12 or int(d) > m_day[int(m)-1]):
+        print("#%d -1" % (test_case))
+    else:
+        print("#%d %s/%s/%s" % (test_case, y, m, d))
