0e8882341f53c16dee893aea3abb9f680a808fd6
SH-indeyong <ohsh1115@gmail.com>
2023-10-02 04:15:51 +0900

    pass

--- D2/2007. 패턴 마디의 길이.py
+++ D2/2007. 패턴 마디의 길이.py
@@ -1,16 +1,20 @@
 T = int(input())
 
-for tc in range(T, T+1):
+for tc in range(1, T+1):
     text = input()
-    t = text[0]
     pattern = []
     next_pattern = []
-    word = []
+    l=0
     
     for i in range(11):
         pattern = text[:i]
         next_pattern = text[i:i*2]
-        if (pattern == next_pattern):
-            word = pattern
-        
-    print("#%d %d" % (tc, len(word)))
+        if (i != 0 and pattern == next_pattern):
+            l = len(pattern)
+            break
+
+    print("#%d %d" % (tc, l))
+
+    '''
+    i=0 인 경우 pattern == next_pattern 만족하므로 예외처리
+    '''
